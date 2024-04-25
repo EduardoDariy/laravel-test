e0eabb9abebdfa84a32c97f7aa81cd4b46ae6b27
Eduard Dariy <eduard.dariy.adci@gmail.com>
2024-04-25 15:12:57 +0500

    added user create method

--- app/app/Http/Controllers/UserController.php
+++ app/app/Http/Controllers/UserController.php
@@ -11,4 +11,15 @@ class UserController extends Controller
         $users = User::all();
         return response()->json($users);
     }
+
+    public function create(Request $request){
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->save();
+        return response()->json([
+            'message' => 'User added'
+        ], 201);
+    }
 }
